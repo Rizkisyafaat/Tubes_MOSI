24c4a47b18b55c6cf833cd36996eb62035720ccf
Rizkisyafaat <rizkisyafaaat@gmail.com>
2020-04-29 22:01:42 +0700

    infeksi dan imunitas

--- Random Walks.py
+++ Random Walks.py
@@ -15,6 +15,7 @@ y_max = 20
 
 x_range = x_max-x_min
 y_range = y_max-y_min
+
 x_position = []
 y_position = []
 
@@ -33,6 +34,30 @@ def updatePosition(x,y):
     elif arah <= 0.8:
         y = y + 1
     return [x,y]
+
+# Boundary
+def boundary(x,y):
+    if (x > x_max):
+        x = x - x_range
+    elif (x < x_min):
+        x = x + x_range
+    elif (y > y_max):
+        y = y - y_range
+    elif (y < y_min):
+        y = y + y_range
+    return[x,y]
+
+# jarak orang sehat dan orang sakit = dekat (ketularan)
+def cek_posisi(x,y,x_position,y_position):
+    status = False
+    i = 0
+    while ((status == False) & (i < jumlah_individu)):
+        if ((status_terinfeksi[i]) & (x_position[i] == x) & (y_position[i] == y)):
+            status = True
+        i = i + 1
+    return status
+
+
 
 for individu in range(jumlah_individu):
     x_position.append(random.randint(x_min,x_max))
@@ -53,3 +78,20 @@ for individu in range(jumlah_individu):
 while (terinfeksi > 0):
     for i in range(jumlah_individu):
         updatePosition(x_position[i],y_position[i])
+
+        x = boundary(updatePosition[0], updatePosition[1]) [0]
+        y = boundary(updatePosition[0], updatePosition[1]) [1]
+
+        if (status_terinfeksi[i]):
+            waktu_infeksi[i] = waktu_infeksi[i] + 1
+
+
+        if (waktu_infeksi[i] > waktu_pemulihan):
+             status_imunitas[i] = True
+             waktu_infeksi[i] = 0
+             terinfeksi = terinfeksi - 1
+
+
+       if (status_imunitas[i]==False & status_terinfeksi[i]==False & cek_posisi(x, y, x_position, y_position)):
+             status_terinfeksi[i] = True
+             terinfeksi = terinfeksi + 1
