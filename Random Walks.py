034c8ee34845fa61f75048d6abbb4d88bb11b2b8
Titankinan <titankinan48@gmail.com>
2020-04-29 20:31:31 +0700

    posisi individu

--- Random Walks.py
+++ Random Walks.py
@@ -1,7 +1,8 @@
-
+import random
 
 #inisialisasi variabel scalar
-jumlah_individu = 200 * 0.05
+jumlah_individu = 200
+terinfeksi = jumlah_individu * 0.05
 probabilitas_bergerak = 0.8
 waktu_pemulihan = 10
 
@@ -14,6 +15,11 @@ y_max = 20
 
 x_range = x_max-x_min
 y_range = y_max-y_min
-x_pos = []
-y_pos = []
+x_position = []
+y_position = []
 
+for individu in range(jumlah_individu):
+    x_position.append(random.randint(x_min,x_max))
+    y_position.append(random.randint(y_min,x_max))
+
+print(x_position[0])
