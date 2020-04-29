53fcb1683c83b43ad8387f195b169c38db85f2c6
Rizkisyafaat <rizkisyafaaat@gmail.com>
2020-04-29 20:13:26 +0700

    langkah 1 inisialisasi

--- Random Walks.py
+++ Random Walks.py
@@ -1,1 +1,19 @@
 
+
+#inisialisasi variabel scalar
+jumlah_individu = 200 * 0.05
+probabilitas_bergerak = 0.8
+waktu_pemulihan = 10
+
+
+#ukuran ruang simulasi
+x_min = 0
+y_min = 0
+x_max = 20
+y_max = 20
+
+x_range = x_max-x_min
+y_range = y_max-y_min
+x_pos = []
+y_pos = []
+
